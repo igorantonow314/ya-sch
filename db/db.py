58bab2f0d5c952cadacbd2574aa0c0f145effd30
igor <igorantonow314@gmail.com>
2022-06-25 23:25:24 +0300

    Changed db.py: using class ShopUnit. Added DB.insert_or_update() with tests

--- db/db.py
+++ db/db.py
@@ -1,30 +1,46 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, DateTime
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import update
+
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import Session
+
+Base = declarative_base()
+
+
+class ShopUnit(Base):
+    __tablename__ = "shop_units"
+
+    id = Column(String, primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
+    date = Column(DateTime, nullable=False)
+    parentId = Column(Integer)
+    type = Column(String, nullable=False)
+    price = Column(Integer)
+
+    def __repr__(self):
+        return f"ShopUnit(id={self.id!r}, name={self.name!r})"
 
 
 class DB:
     def __init__(self, file="shop.db"):
         self.file = file
         self.engine = create_engine(f"sqlite:///{self.file}")
-        self.create_db()
-
-    def create_db(self):
-        meta = MetaData()
-
-        self.shop_units = Table(
-            "shop_units",
-            meta,
-            Column("id", String, primary_key=True, nullable=False),
-            Column("name", String, nullable=False),
-            Column("date", DateTime, nullable=False),
-            Column("parentId", Integer),
-            Column("type", String, nullable=False),
-            Column("price", Integer),
-        )
-
-        meta.create_all(self.engine)
+        Base.metadata.create_all(self.engine)
 
     def insert(self, **data):
-        ins = self.shop_units.insert().values(**data)
-        conn = self.engine.connect()
-        result = conn.execute(ins)
-        return result
+        su = ShopUnit(**data)
+        with Session(self.engine) as session:
+            session.add(su)
+            session.commit()
+
+    def insert_or_update(self, **data):
+        su_id = data.pop("id")
+        with Session(self.engine) as session:
+            if session.get(ShopUnit, su_id):
+                session.execute(
+                    update(ShopUnit).where(ShopUnit.id == su_id).values(**data)
+                )
+            else:
+                session.add(ShopUnit(id=su_id, **data))
+            session.commit()
--- tests/db/test_db.py
+++ tests/db/test_db.py
@@ -6,7 +6,7 @@ import sqlalchemy
 
 from db.db import DB
 
-TEST_DB_NAME = 'test-db.db'
+TEST_DB_NAME = "test-db.db"
 
 
 def test_db_init():
@@ -30,23 +30,47 @@ def test_db_init():
 
 @pytest.fixture
 def db():
-    print('creating new db...')
     if os.path.exists(TEST_DB_NAME):
         os.remove(TEST_DB_NAME)
     return DB(TEST_DB_NAME)
 
 
-def test_db_insert(db):
-    db.insert(
-        id="dldl",
-        name="test",
-        date=datetime.datetime.fromisoformat("2022-06-25T17:53:40"),
-        type="CATEGORY",
-    )
+SHOP_UNIT_EXAMPLES = [
+    {
+        "type": "CATEGORY",
+        "name": "Телевизоры",
+        "id": "1cc0129a-2bfe-474c-9ee6-d435bf5fc8f2",
+        "parentId": "069cb8d7-bbdd-47d3-ad8f-82ef4c269df1",
+        "price": 50999,
+        "date": datetime.datetime.fromisoformat("2022-02-03T15:00:00.000"),
+    },
+    {
+        "type": "OFFER",
+        "name": 'Samson 70" LED UHD Smart',
+        "id": "98883e8f-0507-482f-bce2-2fb306cf6483",
+        "parentId": "1cc0129a-2bfe-474c-9ee6-d435bf5fc8f2",
+        "price": 32999,
+        "date": datetime.datetime.fromisoformat("2022-02-03T12:00:00.000"),
+    },
+]
+
+
+@pytest.mark.parametrize("shop_unit", SHOP_UNIT_EXAMPLES)
+def test_db_insert(db, shop_unit):
+    db.insert(**shop_unit)
     with pytest.raises(sqlalchemy.exc.IntegrityError):
-        db.insert(
-            id="dldl",
-            name="test",
-            date=datetime.datetime.fromisoformat("2022-06-25T17:53:40"),
-            type="CATEGORY",
-        )
+        db.insert(**shop_unit)
+
+
+def test_db_insert_all(db):
+    for shop_unit in SHOP_UNIT_EXAMPLES:
+        db.insert(**shop_unit)
+        with pytest.raises(sqlalchemy.exc.IntegrityError):
+            db.insert(**shop_unit)
+
+
+@pytest.mark.parametrize("shop_unit", SHOP_UNIT_EXAMPLES)
+def test_db_insert_or_update(db, shop_unit):
+    db.insert_or_update(**shop_unit)
+    db.insert_or_update(**shop_unit)
+    db.insert_or_update(**shop_unit)
